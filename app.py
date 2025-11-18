59a2eb2d74f92e0ebb4165b72856c267b802fc11
= <=>
2025-11-17 18:46:20 -0700

    pimpampum

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 from flask import Flask, render_template, request
 import requests
 
+
 app = Flask(__name__)
 
+
 API_KEY = "udJqwbKLuDZTGgIEmmOlAre5tV47sjQ5ichJxxQO"
+
+tipos = {
+"Branded": "Producto de marca comercial",
+"Survey (FNDDS)": "Alimento de encuesta nacional",
+"Foundation": "Alimento base (datos detallados)",
+"SR Legacy": "Base de datos antigua del USDA",
+"Sample": "Muestra analizada en laboratorio"
+}
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -14,12 +25,17 @@ def index():
         url = f"https://api.nal.usda.gov/fdc/v1/foods/search?query={query}&api_key={API_KEY}"
         response = requests.get(url)
 
+
         if response.status_code == 200:
             data = response.json()
             alimentos = data.get("foods", [])
         else:
             alimentos = []
-    return render_template("index.html", alimentos=alimentos, query=query)
+
+
+    return render_template("index.html", alimentos=alimentos, query=query, tipos=tipos)
+
+
 
 
 @app.route("/detalle/<int:fdc_id>")
@@ -27,13 +43,17 @@ def detalle(fdc_id):
     url = f"https://api.nal.usda.gov/fdc/v1/food/{fdc_id}?api_key={API_KEY}"
     response = requests.get(url)
 
+
     if response.status_code == 200:
         data = response.json()
     else:
         data = None
 
+
     return render_template("detalle.html", alimento=data)
 
 
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True)(debug=True)
